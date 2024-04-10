45c93d37bfae2b454790a4620d41135d45f6c36e
TacticsDreams <alexis.fluzin@student.heaj.be>
2024-04-10 20:43:51 +0200

    Step 1 over

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
 
-const App: React.FC = () => {
-    const name: String = 'React';
+const App: FunctionComponent = () => {
+    const [name, setName] = useState<String>('React');
+    const [pokemons, setPokemons] = useState(POKEMONS);
 
-    return (<h1>Hello, {name} !</h1>)
+    return (
+    <><h1>Bonjour, {name} !</h1><p>Il y a couramment {POKEMONS.length} Pokémons dans le PokéDex.</p></>
+)
 }
 
 export default App;
