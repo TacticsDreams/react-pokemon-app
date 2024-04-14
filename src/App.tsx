9f8c44fd78ce4b1ea1dc492a13beb99ba2a92a4d
TacticsDreams <alexis.fluzin@student.heaj.be>
2024-04-14 15:39:46 +0200

    Pokemon hook added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemon';
 
 const App: FunctionComponent = () => {
-    const [name, setName] = useState<String>('React');
-    const [pokemons, setPokemons] = useState(POKEMONS);
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+    useEffect(() =>{
+        setPokemons(POKEMONS);
+    }, []);
+    
     return (
-    <><h1>Bonjour, {name} !</h1><p>Il y a couramment {POKEMONS.length} Pokémons dans le PokéDex.</p></>
+    <div>
+        <h1>Pokédex</h1>
+        <p>Il y a couramment {pokemons.length} Pokémons dans le PokéDex.</p>
+    </div>
 )
 }
 
