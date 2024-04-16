8a906ada59111712e4d26cd456c155de930bba81
TacticsDreams <alexis.fluzin@student.heaj.be>
2024-04-16 21:22:09 +0200

    Navigation mise en place.

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,9 @@
 import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
+import formatDate from '../helpers/format-date';
+import formatType from '../helpers/format-type';
 import './pokemon-card.css';
+import { useHistory } from 'react-router-dom';
 
 type Props = {
     pokemon: Pokemon;
@@ -10,6 +13,7 @@ type Props = {
 const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
 
     const [color, setColor] = useState<string>();
+    const history = useHistory();
 
     const showBorder = () => {
         setColor(borderColor);
@@ -18,16 +22,24 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
     const hideBorder = () => {
         setColor('#f5f5f5');
     }
+
+    const goToPokemon = (id: number) => {
+        history.push(`/pokemons/${id}`);
+    }
+
     return (
-        <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
-          <div className="card horizontal" style={{ borderColor: color}}>
+        <div className="col s6 m4" onClick={() => goToPokemon(pokemon.id)} onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+          <div className="card horizontal" style={{ borderColor: color }}>
             <div className="card-image"> 
               <img src={pokemon.picture} alt={pokemon.name}/>
             </div>
             <div className="card-stacked">
               <div className="card-content">
                 <p>{pokemon.name}</p>
-                <p><small>{pokemon.created.toString()}</small></p>
+                <p><small>{formatDate(pokemon.created)}</small></p>
+                {pokemon.types.map(type => {
+                    return <span key={type} className={formatType(type)}>{type}</span>
+                })}
               </div>
             </div>
           </div> 
